bf151a1a855be75fd5bc052571f3c3d63f58cd27
manzi osee <85952735+manziosee@users.noreply.github.com>
2023-10-04 14:03:39 +0200

    commit #1

---
+++ weather-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="app">
+  <div class="header container h-100 p-5">
+    <h1 class="mb-5">Weather App</h1>
+    <div class="d-flex justify-content-center h-100">
+      <div class="searcher w-50 mx-2">
+        <input type="text" class="input form-control" placeholder="Enter a city">
+      </div>
+      <button class="btn-search btn btn-primary">Search <i class="fas fa-search"></i></button>
+    </div>
+  </div>
+</div>  
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
