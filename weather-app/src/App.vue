a3611d3e0d83e915299f2608434c2c922a06a61e
manzi osee <85952735+manziosee@users.noreply.github.com>
2023-10-05 11:08:04 +0200

    commit #2

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -4,23 +4,55 @@
     <h1 class="mb-5">Weather App</h1>
     <div class="d-flex justify-content-center h-100">
       <div class="searcher w-50 mx-2">
-        <input type="text" class="input form-control" placeholder="Enter a city">
+        <input type="text" class="input form-control" v-model="city" placeholder="Enter a city">
       </div>
       <button class="btn-search btn btn-primary">Search <i class="fas fa-search"></i></button>
     </div>
   </div>
+  <br>
+  <Weather></Weather>
 </div>  
 </template>
 
 <script>
 
+import Weather from './components/Weather.vue';
+
 export default {
   name: 'App',
   components: {
+    Weather
+  },
+  data(){
+    return{
+      city: '',
+      showWeather: false,
+    }
+  },
+  methods:{
+    async searchWeather (){
+      this.showWeather
+      await this.$nextTick();
+      this.showWeather = true;
+    }
   }
 }
 </script>
 
 <style>
+body{
+  background-color: #121212 !important;
+}
+.header{
+  background-color: #212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  margin-top: 5rem;
+}
 
+.btn-search {
+  background-image: linear-gradient(to right, cyan, magenta);
+}
 </style>
---
+++ weather-app/src/components/Weather.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="container p-0">
+    <div class="d-flex">
+        <div class="card main-div w-100">
+            <div class="p-3">
+                <h2 class="mb-1 day">Tuesday</h2>
+                <p class="text-light date mb-0">date</p>
+                <small>time</small>
+                <h2 class="place"><i class="fa fa-location">Rio<small>country</small></i></h2>
+                <div class="temp">
+                    <h1 class="weather-temp">19&deg;</h1>
+                    <h2 class="text-light">description</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card card-2 w-100">
+        <table class="m-4">
+            <tbody>
+                <tr>
+                    <th>Sea Level</th>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th>Sea Level</th>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th>Sea Level</th>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+        <div id="div_Form" class="d-flex m-3 justify-content-center">
+            <form action="">
+                <input type="button" value="Change Location" class="btn change-btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>    
+    </template>
+    
+    <script>
+    
+    export default {
+        name: 'myWeather',
+  components: {
+    
+  },    
+    }
+    </script>
+    
+    <style>
+    body{
+        background-color: #343d4b;
+    }
+    .weather-temp{
+        margin: 0;
+        font-weight: 700;
+        font-size: 4em;
+    }
+    h2.mb-1.day {
+        font-size: 3rem;
+        font-weight: 400;
+    }
+    .main-div{
+        
+    }
+  
+    </style>
+    
